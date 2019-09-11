a42680d7c960b41271be032859ec98c765fbc1f7
azuline <azuline@riseup.net>
2019-09-10 20:05:21 -0400

    more changes

--- lib/ricochet_robots/socket_handler.ex
+++ lib/ricochet_robots/socket_handler.ex
@@ -21,22 +21,22 @@ defmodule RicochetRobots.SocketHandler do
   end
 
   @impl true
-  def websocket_handle({:text, "ping"}, state) do
+  def websocket_handle({:text, message}, state) do
+    data = Poison.decode!(message)
+    websocket_handle({:json, message}, state)
+    {:ok, state}
+  end
+
+  @impl true
+  def websocket_handle({:json, {"action": "ping"}, state) do
     Logger.debug("ping pong")
     {:reply, {:text, "pong"}, state}
   end
 
   @impl true
-  def websocket_handle({:set_nick, nick}, state) do
+  def websocket_handle({:json, {"action": "set_nick", "nick": nick}, state) do
     state.player = Map.put(state.player, :nickname, nick)
     {:reply, {:text "success"}, state}
-  end
-
-  @impl true
-  def websocket_handle({:text, message}, state) do
-    data = Poison.decode!(message)
-    websocket_handle({:json, message}, state)
-    {:ok, state}
   end
 
   @impl true
@@ -46,26 +46,26 @@ defmodule RicochetRobots.SocketHandler do
   end
 
   @impl true
-  def websocket_handle({:json, %{"action": "join_room"} = data, state) do
+  def websocket_handle({:json, %{"action": "join_room"}, state) do
     # Create the player, have it join the room, add player to state.
     # Max # of players per game check?
     # Log to chat.
   end
 
   @impl true
-  def websocket_handle({:json, %{"action": "new_game"} = data, state) do
+  def websocket_handle({:json, %{"action": "new_game"}, state) do
     # Create the board and randomize the setup; modify game state.
     # Log to chat.
   end
 
   @impl true
-  def websocket_handle({:json, %{"action": "submit_solution"} = data, state) do
+  def websocket_handle({:json, %{"action": "submit_solution", "solution": solution}, state) do
     # Check solution with solver
     # Log submission and result to chat.
   end
 
   @impl true
-  def websocket_handle({:json, %{"action": "send_chat_message"} = data, state) do
+  def websocket_handle({:json, %{"action": "send_chat_message", "message": message}, state) do
     # Relay to chat.
   end
 
