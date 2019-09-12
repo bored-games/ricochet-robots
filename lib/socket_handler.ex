9fb4ead49ee1e4cf7f9705961cac07b725db95bc
gg314 <gg314@users.noreply.github.com>
2019-09-11 19:20:38 -0600

    add documentation, TODOs

--- lib/socket_handler.ex
+++ lib/socket_handler.ex
@@ -1,19 +1,24 @@
 defmodule Player do
-
   defstruct username: nil, color: "#cccccc", score: 0, owner: false, muted: false, joined: nil
-
 end
+
 
 defmodule RicochetRobots.SocketHandler do
   @behaviour :cowboy_websocket
-  @timeout 90000
+  @timeout 90000 # 90 sec. socket timeout
 
   import Bitwise
   require Logger
   require Player
 
+  # doesn't work?
   # @system_user = %{username: "System", color: "#fff", score: 0, owner: false, muted: false}
 
+  # TODO: visual_board (16x16 grid representing CSS squares) should be in parent module
+  # TODO: boundary_board (33x33 grid representing open spaces and walls) should be in parent module
+  # TODO: robots list should be in parent module
+  # TODO: goal-symbols list should be in parent module.
+  # TODO: users list should be in parent module AND somehow tied to registry to show active connections...
   def init(request, _state) do
     { visual_board, boundary_board, goals } = build_board()
     state = %{ registry_key: request.path,
@@ -30,6 +35,7 @@ defmodule RicochetRobots.SocketHandler do
     {:cowboy_websocket, request, state, %{ idle_timeout: @timeout } }
   end
 
+  @doc "websocket_init: functions that must be called after init()"
   def websocket_init(state) do
     Registry.RicochetRobots
     |> Registry.register(state.registry_key, {})
@@ -37,7 +43,8 @@ defmodule RicochetRobots.SocketHandler do
     {:ok, state}
   end
 
-
+  @doc "Route valid socket messages to other websocket_handle() functions"
+  # TODO: if it is valid json, forward it along. Otherwise, handle the error
   def websocket_handle({:text, json}, state) do
     payload = Poison.decode!(json)
     websocket_handle({:json, payload["code"], payload["content"]}, state)
@@ -46,7 +53,7 @@ defmodule RicochetRobots.SocketHandler do
   @doc "ping : Message every 90 sec or the connection will be closed. Responds with pong."
   def websocket_handle({:json, 001, _content}, state) do
     Logger.debug("[Ping] ")
-    response = Poison.encode!( %{ content: "pong!", code: 001 }  )
+    response = Poison.encode!( %{ content: "pong!", code: 001 } )
     {:reply, {:text, response}, state}
   end
 
@@ -80,6 +87,7 @@ defmodule RicochetRobots.SocketHandler do
 
   @doc "new_user : need to send out user initialization info to client,
   and new user message, scoreboard to all users"
+  # TODO: rewrite this when module hierarchy is sorted out!
   def websocket_handle({:json, 200, _content}, state) do
     Logger.debug("[New user]: " <> state[:user].username)
     users = [state[:user] | state[:users] ]
@@ -167,30 +175,30 @@ defmodule RicochetRobots.SocketHandler do
   end
 
 
-
-
-  @doc "_ : handle all other opcodes as unknown"
+  @doc "_ : handle all other opcodes as unknown."
   def websocket_handle({:json, opcode, _}, state) do
     Logger.debug("[Unhandled code] " <> Integer.to_string(opcode) )
     {:reply, {:text, "Got some unhandled code?"}, state}
   end
 
 
-
+  @doc "websocket_info handles some messages on their way out..."
   def websocket_info({:json, opcode, content}, state) do
       Logger.debug("[Send] " <> Integer.to_string(opcode) )
       data = Poison.encode!( %{ content: content, code: opcode } )
       {:reply, {:text, data}, state}
     end
 
-    def websocket_info(info, state) do
-    #  IO.inspect(info)
-      {:reply, {:text, info}, state}
-    end
+  @doc "Handle all other messages on their way out to clients."
+  def websocket_info(info, state) do
+  #  IO.inspect(info)
+    {:reply, {:text, info}, state}
+  end
 
 
 
   @doc "Return a new user with unique, randomized name"
+  # TODO: verify this doesn't permit duplicate names after switching datastructures
   @spec new_user([Player]) :: Player
   def new_user(users) do
     arr1 = ["Robot", "Doctor", "Puzzle", "Automaton", "Data", "Buzz", "Infinity", "Cyborg", "Android", "Electro", "Robo", "Battery", "Beep" ];
@@ -212,21 +220,7 @@ defmodule RicochetRobots.SocketHandler do
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  # TODO: use these or get rid of them? When is a type better than a defstruct?
   @typedoc "User: { username: String, color: String, score: integer }"
   @type user_t :: %{ username: String.t, color: String.t, score: integer, datestr: DateTime.t }
 
@@ -253,6 +247,7 @@ defmodule RicochetRobots.SocketHandler do
     add_robot("silver", robots)
   end
 
+  # Given color, list of previous robots, add a single 'color' robot to an unoccupied square
   @spec add_robot( String.t , [ robot_t] ) :: [ robot_t ]
   defp add_robot(color, robots) do
     open_squares = [0, 1, 2, 3, 4, 5, 6, 9, 10, 11, 12, 13, 14, 15]
@@ -260,12 +255,13 @@ defmodule RicochetRobots.SocketHandler do
     [ %{ pos: List.first(rlist), color: color, moves: ["up", "left", "down", "right"] } | robots ]
   end
 
-  @doc "Return a randomized board, it's visual map, and corresponding goal positions."
+  @doc "Return a randomized boundary board, its visual map, and corresponding goal positions."
   @spec build_board() :: { map, map, [ goal_t ] }
   def build_board() do
     goal_symbols = Enum.shuffle(["RedMoon","GreenMoon","BlueMoon","YellowMoon","RedPlanet","GreenPlanet","BluePlanet","YellowPlanet","GreenCross","RedCross","BlueCross","YellowCross","RedGear","GreenGear","BlueGear","YellowGear"])
     goal_active = Enum.shuffle([true,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false])
 
+    # I'm so sorry
     a = %{
       0  => %{ 0=>1, 1=>1, 2=>1, 3=>1, 4=>1, 5=>1, 6=>1, 7=>1, 8=>1, 9=>1,10=>1,11=>1,12=>1,13=>1,14=>1,15=>1,16=>1,17=>1,18=>1,19=>1,20=>1,21=>1,22=>1,23=>1,24=>1,25=>1,26=>1,27=>1,28=>1,29=>1,30=>1,31=>1,32=>1},
       1  => %{ 0=>1, 1=>0, 2=>0, 3=>0, 4=>0, 5=>0, 6=>0, 7=>0, 8=>0, 9=>0,10=>0,11=>0,12=>0,13=>0,14=>0,15=>0,16=>0,17=>0,18=>0,19=>0,20=>0,21=>0,22=>0,23=>0,24=>0,25=>0,26=>0,27=>0,28=>0,29=>0,30=>0,31=>0,32=>1},
@@ -312,9 +308,9 @@ defmodule RicochetRobots.SocketHandler do
     a = put_in a[ Enum.random([4,6,8,10,12,14]) ][31], 1
     a = put_in a[ Enum.random([18,20,22,24,26,28]) ][31], 1
 
+    # TODO: ensure L's aren't too close to |'s?
     # four "L"s per quadrant
-    #rlist = rand_distant_pairs([4, 6, 8, 10, 12, 14], [2, 4, 6, 8, 10, 12], [])
-    rlist = [ {4, 2} ]
+    rlist = rand_distant_pairs([4, 6, 8, 10, 12, 14], [2, 4, 6, 8, 10, 12], [])
     {a, goals} = add_L1(a, List.first(rlist), Enum.fetch!(goal_symbols, 0), Enum.fetch!(goal_active, 0), [] )
     rlist = rand_distant_pairs([2, 4, 6, 8, 10, 12], [2, 4, 6, 8, 10, 12], rlist)
     {a, goals} = add_L2(a, List.first(rlist), Enum.fetch!(goal_symbols, 1), Enum.fetch!(goal_active, 1), goals )
@@ -349,10 +345,9 @@ defmodule RicochetRobots.SocketHandler do
     {a, goals} = add_L3(a, List.first(rlist), Enum.fetch!(goal_symbols, 14), Enum.fetch!(goal_active, 14), goals )
     rlist = rand_distant_pairs([20, 22, 24, 26, 28, 30], [20, 22, 24, 26, 28, 30], rlist)
     {a, goals} = add_L4(a, List.first(rlist), Enum.fetch!(goal_symbols, 15), Enum.fetch!(goal_active, 15), goals )
-
     ############################################
 
-
+    # visual_map init:
     b = %{
       0  => %{ 0=>0, 1=>0, 2=>0, 3=>0, 4=>0, 5=>0, 6=>0, 7=>0, 8=>0, 9=>0,10=>0,11=>0,12=>0,13=>0,14=>0,15=>0},
       1  => %{ 0=>0, 1=>0, 2=>0, 3=>0, 4=>0, 5=>0, 6=>0, 7=>0, 8=>0, 9=>0,10=>0,11=>0,12=>0,13=>0,14=>0,15=>0},
@@ -371,11 +366,10 @@ defmodule RicochetRobots.SocketHandler do
       14 => %{ 0=>0, 1=>0, 2=>0, 3=>0, 4=>0, 5=>0, 6=>0, 7=>0, 8=>0, 9=>0,10=>0,11=>0,12=>0,13=>0,14=>0,15=>0},
       15 => %{ 0=>0, 1=>0, 2=>0, 3=>0, 4=>0, 5=>0, 6=>0, 7=>0, 8=>0, 9=>0,10=>0,11=>0,12=>0,13=>0,14=>0,15=>0},
     }
-
     b = populate_rows(a, b, 15)
 
-
-    b = put_in b[7][7], 256  # put in final special blocks into center 4 squares
+    # put in final special blocks into center 4 squares
+    b = put_in b[7][7], 256
     b = put_in b[7][8], 257
     b = put_in b[8][7], 258
     b = put_in b[8][8], 259
@@ -383,6 +377,7 @@ defmodule RicochetRobots.SocketHandler do
     { b, a, goals }
   end
 
+  # Add L
   @spec add_L1( map, {integer, integer}, String.t, boolean, [ goal_t] ) :: { map, [goal_t] }
   defp add_L1(a, {row, col}, goal_string, goal_active, goals) do
     a = put_in a[row][col], 1
@@ -391,6 +386,7 @@ defmodule RicochetRobots.SocketHandler do
     { a, [ %{pos: %{y: div(row-1, 2), x: div(col+1, 2)}, symbol: goal_string, active: goal_active } | goals ] }
   end
 
+  # Add L, rotated 90 deg CW
   @spec add_L2( map, {integer, integer}, String.t, boolean, [ goal_t] ) :: { map, [goal_t] }
   defp add_L2(a, {row, col}, goal_string, goal_active, goals) do
     a = put_in a[row][col], 1
@@ -399,6 +395,7 @@ defmodule RicochetRobots.SocketHandler do
     { a, [ %{pos: %{y: div(row+1, 2), x: div(col+1, 2)}, symbol: goal_string, active: goal_active } | goals ] }
   end
 
+  # Add L, rotated 180 deg
   @spec add_L3( map, {integer, integer}, String.t, boolean, [ goal_t] ) :: { map, [goal_t] }
   defp add_L3(a, {row, col}, goal_string, goal_active, goals) do
     a = put_in a[row][col], 1
@@ -407,6 +404,7 @@ defmodule RicochetRobots.SocketHandler do
     { a, [ %{pos: %{y: div(row+1, 2), x: div(col-1, 2)}, symbol: goal_string, active: goal_active } | goals ] }
   end
 
+  # Add L, rotated 270 deg CW
   @spec add_L4( map, {integer, integer}, String.t, boolean, [ goal_t] ) :: { map, [goal_t] }
   defp add_L4(a, {row, col}, goal_string, goal_active, goals) do
     a = put_in a[row][col], 1
@@ -415,6 +413,8 @@ defmodule RicochetRobots.SocketHandler do
     { a, [ %{pos: %{y: div(row-1, 2), x: div(col-1, 2)}, symbol: goal_string, active: goal_active } | goals ] }
   end
 
+  # Populate a row of visual_board (b) based on boundary board (a)
+  # TODO: how do Elixir people usually write this + the next function?
   defp populate_rows(a, b, row) when row <= 0 do
     populate_cols(a, b, row, 15)
   end
@@ -424,21 +424,14 @@ defmodule RicochetRobots.SocketHandler do
     populate_rows(a, b, row-1)
   end
 
-  #TOP = 1
-  #RIG = 2
-  #BOT = 4
-  #LEF = 8
-  #TRT = 16
-  #BRT = 32
-  #BLT = 64
-  #TLT = 128
-
+  #TOP = 1    RIG = 2    BOT = 4    LEF = 8
+  #TRT = 16   BRT = 32   BLT = 64   TLT = 128
+  # Find the bordering cells of b[row][col] in the boundary_board (a) and stuff in the correct integer representation for frontend presentation
   defp populate_cols(a, b, row, col) when col <= 0 do
     cc = (2*col+1)
     rr = (2*row+1)
     put_in b[row][col], ( (a[rr-1][cc]*1) ||| (a[rr][cc+1]*2) ||| (a[rr+1][cc]*4) ||| (a[rr][cc-1]*8) ||| (a[rr-1][cc+1]*16) ||| (a[rr+1][cc+1]*32) ||| (a[rr+1][cc-1]*64) ||| (a[rr-1][cc-1]*128) )
   end
-
   defp populate_cols(a, b, row, col) do
     cc = (2*col+1)
     rr = (2*row+1)
@@ -446,6 +439,7 @@ defmodule RicochetRobots.SocketHandler do
     populate_cols(a, b, row, col-1)
   end
 
+  # TODO: write this with position_t type
   @doc "Given a list of {int, int} pairs not to repeat, and two arrays to choose new tuples from, return a list with a new unique tuple"
   @spec rand_unique_pairs([integer], [integer], [ %{x: integer, y: integer}]) :: [ %{x: integer, y: integer} ]
   def rand_unique_pairs(rs, cs, avoids, cnt \\ 0) do
@@ -461,6 +455,7 @@ defmodule RicochetRobots.SocketHandler do
     end
   end
 
+  # TODO: write this with position_t type
   @doc "Given a list of {int, int} pairs to avoid, and two arrays to choose new tuples from, return a list with a new tuple at least 2 distance away"
   @spec rand_distant_pairs([integer], [integer], [{integer, integer}]) :: [{integer, integer}]
   def rand_distant_pairs(rs, cs, avoids, cnt \\ 0) do
@@ -476,12 +471,12 @@ defmodule RicochetRobots.SocketHandler do
     end
   end
 
+  # TODO: write this with position_t type
   @doc "Take {x1, y1} and {x2, y2}; is the distance between them more than 2.0 (on condensed board; 4.0 on boundary_board)?"
   @spec dist_under_2?({integer, integer}, {integer, integer}) :: boolean
   def dist_under_2?({x1, y1}, {x2, y2}) do
     ((y1-y2)*(y1-y2) + (x1-x2)*(x1-x2)) <= 16.0
   end
-
 
 end
 
