0c26bbcd4175b459967972ad4c460e425d32829d
gg314 <54562746+gg314@users.noreply.github.com>
2021-11-05 09:45:43 -0600

    last try

--- config/config.exs
+++ config/config.exs
@@ -14,7 +14,7 @@ config :gameboy, Gameboy.RicochetRobots.Repo,
 config :gameboy, ecto_repos: [Gameboy.RicochetRobots.Repo]
 
 config :gameboy, Gameboy,
-  http: [port: {:system, "PORT"}], # Possibly not needed, but doesn't hurt
+  http: [port: 4000], # Possibly not needed, but doesn't hurt
   url: [host: System.get_env("APP_NAME") <> ".gigalixirapp.com", port: 443],
   secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE"),
   server: true
