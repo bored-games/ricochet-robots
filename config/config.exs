fbce4b0d6bafd8e23f6dc080bc761e72ddfaecf1
gg314 <54562746+gg314@users.noreply.github.com>
2021-11-06 15:18:50 -0600

    clean up for gigalixer deploy

--- config/config.exs
+++ config/config.exs
@@ -4,13 +4,6 @@ config :gameboy, Gameboy.RicochetRobots.Repo,
   database: "ricochetrobots_repo",
   username: "postgres",
   password: "notarealpassword",
-  hostname: "localhost",
-  adapter: Ecto.Adapters.Postgres,
-  url: "${DATABASE_URL}",
-  database: "",
-  ssl: true,
-  pool_size: 2 # Free tier db only allows 4 connections. Rolling deploys need pool_size*(n+1) connections where n is the number of app replicas.
+  hostname: "localhost"
 
 config :gameboy, ecto_repos: [Gameboy.RicochetRobots.Repo]
-
-config :gen_tcp_accept_and_close, port: 4000
