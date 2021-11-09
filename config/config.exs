400b752c61165ec3cbb5ae293b446f3a3aa649d7
gg314 <54562746+gg314@users.noreply.github.com>
2021-11-09 11:34:02 -0700

    test env var

--- config/config.exs
+++ config/config.exs
@@ -1,10 +1,9 @@
 import Config
 
 config :gameboy, Gameboy.RicochetRobots.Repo,
-  adapter: Ecto.Adapters.Postgres,
-  url: {:system, "DATABASE_URL"},
-  database: "",
-  ssl: true,
-  pool_size: 2
+  database: "ricochetrobots_repo",
+  username: "postgres",
+  password: "redf",
+  hostname: "localhost"
 
 config :gameboy, ecto_repos: [Gameboy.RicochetRobots.Repo]
--- lib/gameboy/router.ex
+++ lib/gameboy/router.ex
@@ -2,12 +2,14 @@ defmodule Gameboy.Router do
   @moduledoc false
 
   use Plug.Router
+  use Logger
 
   plug(Plug.Static, at: "/", from: {:ricochet_robots, "priv/"})
   plug(:match)
   plug(:dispatch)
 
   match _ do
+    Logger.debug("AAAAAAAAAAAAAA #{inspect({:system, "DATABASE_URL"}}............")
     send_resp(conn, 200, "Success")
   end
 end
