43678c1985df26dde0f249265c506856d6a516f6
gg314 <54562746+gg314@users.noreply.github.com>
2021-11-13 10:08:11 -0700

    temporarily remove sec-websocket-protocol

--- lib/gameboy/socket_handler.ex
+++ lib/gameboy/socket_handler.ex
@@ -26,7 +26,7 @@ defmodule Gameboy.SocketHandler do
   @impl true
   def init(request, _state) do
     state = %__MODULE__{
-      player_name: Player.new(self(), request.headers["sec-websocket-protocol"]),
+      player_name: Player.new(self(), "1234"), # 1234 = request.headers["sec-websocket-protocol"]
       room_name: nil
     }
 
